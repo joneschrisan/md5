d227a6b6d741f4c55df8f2a8a3d3b576469ce40e
Chris 'CJ' Jones <joneschrisan@users.noreply.github.com>
2016-11-28 15:00:58 +0000

    Up dated to version 1
    Added options and hashing of files as well as strings.

--- md5.pl
+++ md5.pl
@@ -1,37 +1,94 @@
-#!/usr/bin/perl
+use Getopt::Std;
+use Digest::MD5::File qw(md5 md5_hex md5_base64 file_md5 file_md5_hex file_md5_base64);
 
-use Switch;
-use Digest::MD5 qw(md5 md5_hex md5_base64);
+$version = "1.0.00";
 
 sub run {
     $out = "";
     
-    switch (@_[0]) {
-        case "plain" { $out = md5(@_[1]); }
-        case "base64" { $out = md5_base64(@_[1]); }
-        case "hex" {$out = md5_hex(@_[1]);  }
-        else { $out = md5_hex(@_[1]); }
-    }
+    if (@_[0] eq "p") {
+    	if (@_[2] eq "f") {
+    		$out = file_md5(@_[1]);
+    	} else {
+    		$out = md5(@_[1]);
+    	}
+	} elsif (@_[0] eq "b") {
+		if (@_[2] eq "f") {
+    		$out = file_md5_base64(@_[1]);
+    	} else {
+    		$out = md5_base64(@_[1]);
+    	}
+	} elsif (@_[0] eq "x") {
+		if (@_[2] eq "f") {
+    		$out = file_md5_hex(@_[1]);
+    	} else {
+    		$out = md5_hex(@_[1]);
+    	}
+	} else {
+		if (@_[2] eq "f") {
+    		$out = file_md5_hex(@_[1]);
+    	} else {
+    		$out = md5_hex(@_[1]);
+    	}
+	}
     
     return $out;
 }
 
 @types = ('plain', 'hex', base64);
 
-$len = scalar(keys @ARGV);
+getopts("fvhpbx", \%opts);
 
-if ($len eq 0) {
-    die("An argument must be provided\n");
+print grep { /h/ } %opts;
+print "\n";
+
+if (grep { /h/ } %opts) {
+	print "\n";
+	print "MD5 - " . $version . "\n";
+	print "\n";
+	print "Author Chris 'CJ' Jones\n";
+	print "\n";
+	print "Copywrite Chris 'CJ' Jones 2016";
+	print "\n";
+	print "Website https://github.com/joneschrisan/md5\n";
+	print "\n";
+	print "Usage\n";
+	print "   md5 [options] [string [string [...]]]\n";
+	print "\n";
+	print "h Print this help file\n";
+	print "v Print version info\n";
+	print "f String(s) is/are files\n";
+	print "p Use plain hash\n";
+	print "b Use base64 hash\n";
+	print "x Use HEX hash (default)\n";
+	exit();
+} elsif (grep { /v/ } %opts) {
+	print $version;
+	print "\n";
+	exit();
 } else {
-    $first = shift @ARGV;
-    if (grep $_ eq $first, @types) {
-        # Do nothing.
-    } else {
-        unshift @ARGV, $first;
-        $first = NUL;
-    }
-    
-    foreach $arg (@ARGV) {
-        print run($first, $arg) . "\n";
-    }
+	$len = scalar(keys @ARGV);
+
+	if ($len eq 0) {
+		die("An argument must be provided\n");
+	}
+	
+	$opt = "x";
+	if (grep { /p/ } %opts) {
+		$opt = "p";
+	} elsif (grep { /b/ } %opts) {
+		$opt = "b";
+	}
+	
+	if (grep { /f/ } %opts) {
+		foreach $arg (@ARGV) {
+			if (-e $arg) {
+				print run($opt, $arg, "f") . "\n";
+			}
+		}
+	} else {
+		foreach $arg (@ARGV) {
+		    print run($opt, $arg) . "\n";
+		}
+	}
 }
